0c0e7603aa155fcd82bbcbe6fff31586ec814f8a
Kazuyuki Honda <hakobera@gmail.com>
2014-02-25 17:27:23 +0900

    Implement version command

--- lib/ebfly/cli.rb
+++ lib/ebfly/cli.rb
@@ -2,12 +2,18 @@ require "thor"
 require "aws-sdk"
 
 require_relative 'ebfly'
+require_relative 'version'
 require_relative 'command/app'
 require_relative 'command/env'
 require_relative 'command/config'
 
 module Ebfly
   class CLI < Thor
+    desc "version", "show version"
+    def version
+      puts "Ebfly #{Ebfly::VERSION}"
+    end
+
     desc "app SUBCOMMAND ...ARGS", "manage application"
     subcommand "app", App
 
