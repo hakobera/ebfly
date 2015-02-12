512aa0d8ac6cb636a1722c45d143e61695646d83
Kazuyuki Honda <hakobera@gmail.com>
2015-02-12 16:46:19 +0900

    Use explicitly use aws-sdk-v1

--- test/create_all.sh
+++ test/create_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./bin/ebfly app create ebfly-test
+bundle exec ./bin/ebfly app create ebfly-test
 
-for ss in docker09 docker10 nodejs php54 php55 python26 python27 ruby19 ruby20 ruby20-puma
+for ss in docker09 docker10 docker13 nodejs php54 php55 python26 python27 ruby19 ruby20 ruby20-puma ruby21 ruby21-puma
 do
-  ./bin/ebfly env create $ss -s $ss -a ebfly-test
+  bundle exec ./bin/ebfly env create $ss -s $ss -a ebfly-test
 done
